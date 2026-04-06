9015b97a448e8c19a14659c380a44249323382f7
Minh Tu <nguyenbuiminhtu.9a9@gmail.com>
2026-04-06 21:07:36 +0700

    trang chủ, trang chi tiết và tìm theo thể loại

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -1,5 +1,20 @@
-<x-movie-layout>
-    <x-slot name="title">
-        Movie
-    </x-slot>
+<x-movie-layout :genre="$theloai">
+    <x-slot name='title'>Movie</x-slot>
+    <div id='movie-view-div'>
+        @include('movie.movie_view')
+    </div>
+    <script>
+        $(document).ready(function(){
+            $(document).on("click", ".menu-the-loai", function(e){
+                e.preventDefault();
+                let the_loai = $(this).attr("the_loai");
+                $.ajax({
+                    type: "POST",
+                    url: "{{route('movieview')}}",
+                    data: { "_token": "{{ csrf_token() }}", "the_loai": the_loai },
+                    success: function(data) { $("#movie-view-div").html(data); }
+                });
+            });
+        });
+    </script>
 </x-movie-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/', 'App\Http\Controllers\MovieController@index');
+Route::post('/movieview', 'App\Http\Controllers\MovieController@movieview')->name('movieview');
+Route::get('movie/chitiet/{id}', 'App\Http\Controllers\MovieController@chitiet');
+Route::post('/timkiem', 'App\Http\Controllers\MovieController@timkiem')->name('timkiem');
+Route::get('/theloai/{id}', 'App\Http\Controllers\MovieController@theloai');
