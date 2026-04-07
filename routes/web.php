46c9d6b53b6ec40d03fd891a38b4d588ecd23e78
tram-ntb <030239230254@st.buh.edu.vn>
2026-04-07 09:47:56 +0700

    sửa

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\MovieController@index');
 Route::get('/theloai/{id}', 'App\Http\Controllers\MovieController@theloai');
+Route::post('/movieview', 'App\Http\Controllers\MovieController@movieview')->name('movieview');
 Route::get('/chitiet/{id}', [MovieControllerNe::class, 'chitiet']);
 Route::match(['get', 'post'], '/timkiem', [App\Http\Controllers\MovieController::class, 'TimKiem']);
 
